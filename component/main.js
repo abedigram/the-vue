b5edc1caab65c09f67e7a41fb77547a58874fd05
mohammadBagher <mbapost@yahoo.com>
2020-08-12 17:14:04 +0430

    nested components

--- component/main.js
+++ component/main.js
@@ -1,3 +1,23 @@
+Vue.component('task-list', {
+    template: `
+        <div>
+            <task v-for="task in tasks" v-text="task.description"></task>
+        </div>
+    `,
+    data(){
+        return{
+            tasks: [
+                { description: 'Go to the store', completed: true },
+                { description: 'Finish screencast', completed: false },
+                { description: 'Make donation', completed: false },
+                { description: 'Clear inbox', completed: false },
+                { description: 'Make dinner', completed: false },
+                { description: 'Clean room', completed: true }
+            ]
+        }
+    }
+})
+
 Vue.component('task',{
     template:
     `<li> 
